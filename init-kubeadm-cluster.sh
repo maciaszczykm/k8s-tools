e7615c4c47bc6401f885cf37175c79067196a31f
Marcin Maciaszczyk <marcin.maciaszczyk@ts.fujitsu.com>
2017-07-27 15:12:23 +0200

    Update kubeadm init script

--- init-kubeadm-cluster.sh
+++ init-kubeadm-cluster.sh
@@ -5,12 +5,19 @@ sudo kubeadm reset
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 
 # Setup kubectl.
-sudo cp /etc/kubernetes/admin.conf /home/maciaszczykm/.kube/config
-sudo chown -R maciaszczykm /home/maciaszczykm
+sudo cp /etc/kubernetes/admin.conf ${HOME}/.kube/config
+sudo chown -R ${USER} ${HOME}
 
-# Taint master node for development usage.
+# Taint master for development usage.
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
-# Deploy flannel.
-kubectl create -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-kubectl create -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel-rbac.yml
+# Deploy Flannel.
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel-rbac.yml
+
+# Deploy Dashboard.
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/kubernetes-dashboard.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/kubernetes-dashboard-head.yaml
+
+# Deploy other resources for testing.
+# TODO
