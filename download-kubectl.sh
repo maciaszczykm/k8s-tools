cbe769f2a3ff3ed727557d872e2195c0ec055152
maciaszczykm <maciaszczykm@icloud.com>
2017-10-26 13:30:03 +0200

    Fix script for macOS

--- download-kubectl.sh
+++ download-kubectl.sh
@@ -3,10 +3,10 @@
 source setup-logger.sh
 
 K8S_VERSION=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
-UNAME=$(uname)
-KUBECTL_DOWNLOAD_LINK=https://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/${UNAME,,}/amd64/kubectl
+ARCH=$(uname | awk '{print tolower($0)}')
+KUBECTL_DOWNLOAD_LINK=https://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/${ARCH}/amd64/kubectl
 
-log::info "Downloading kubectl ${K8S_VERSION} for ${UNAME,,}"
+log::info "Downloading kubectl ${K8S_VERSION} for ${ARCH}"
 curl -LO ${KUBECTL_DOWNLOAD_LINK} --progress-bar
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
--- init-dind-cluster.sh
+++ init-dind-cluster.sh
@@ -3,18 +3,25 @@
 source setup-logger.sh
 
 SCRIPT_K8S_VERSION=v1.8
-REMOTE_SCRIPT=https://raw.githubusercontent.com/Mirantis/kubeadm-dind-cluster/master/fixed/dind-cluster-${SCRIPT_VERSION}.sh
-log::info "Using remote scipt from ${REMOTE_SCRIPT}"
+REMOTE_SCRIPT=https://raw.githubusercontent.com/Mirantis/kubeadm-dind-cluster/master/fixed/dind-cluster-${SCRIPT_K8S_VERSION}.sh
+LOCAL_SCRIPT=./dind-cluster-${SCRIPT_K8S_VERSION}.sh
+
+log::info "Downloading remote scipt from ${REMOTE_SCRIPT}"
+curl -LO ${REMOTE_SCRIPT} --progress-bar
+chmod +x ${LOCAL_SCRIPT}
 
 log::info "Cleaning after previous cluster"
-curl -s ${REMOTE_SCRIPT} | bash /dev/stdin down
-curl -s ${REMOTE_SCRIPT} | bash /dev/stdin clean
+${LOCAL_SCRIPT} down
+${LOCAL_SCRIPT} clean
 
 source download-kubectl.sh
 
 log::info "Initializing cluster"
-curl -s ${REMOTE_SCRIPT} | bash /dev/stdin up
+${LOCAL_SCRIPT} up
 
 source deploy-resources.sh
+
+log::info "Deleting local copy of startup script"
+rm ${LOCAL_SCRIPT}
 
 log::info "Cluster is ready"
